a3481d00ea1be9d1c7a37d1e782e9abd7c16ceed
Samuel Selvan <prabhu.samuel@gmail.com>
2024-07-22 08:51:25 -0700

    Add files via upload

---
+++ models/llama3_1/download.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 3.1 Community License Agreement.
+
+set -e
+
+read -p "Enter the URL from email: " PRESIGNED_URL
+ALL_MODELS="meta-llama-3.1-405b-instruct-mp16,meta-llama-3.1-405b-instruct-mp8,meta-llama-3.1-405b-instruct,meta-llama-3.1-405b-mp16,meta-llama-3.1-405b-mp8,meta-llama-3.1-405b,meta-llama-3.1-70b-instruct,meta-llama-3.1-70b,meta-llama-3.1-8b-instruct,meta-llama-3.1-8b,meta-llama-guard-3-8b-int8-hf,meta-llama-guard-3-8b,prompt-guard"
+printf "\n **** Available models to download: ***\n"
+for MODEL in ${ALL_MODELS//,/ }
+do
+    printf " -  ${MODEL}\n"
+done
+read -p "Enter the list of models to download without spaces or press Enter for all: " SELECTED_MODELS
+TARGET_FOLDER="."             # where all files should end up
+mkdir -p ${TARGET_FOLDER}
+
+if [[ $SELECTED_MODELS == "" ]]; then
+    SELECTED_MODELS=${ALL_MODELS}
+fi
+
+printf "Downloading LICENSE and Acceptable Usage Policy\n"
+wget --continue ${PRESIGNED_URL/'*'/"LICENSE"} -O ${TARGET_FOLDER}"/LICENSE"
+wget --continue ${PRESIGNED_URL/'*'/"USE_POLICY.md"} -O ${TARGET_FOLDER}"/USE_POLICY.md"
+
+for m in ${SELECTED_MODELS//,/ }
+do
+
+    ADDITIONAL_FILES=""
+    TOKENIZER_MODEL=1
+    if [[ $m == "meta-llama-3.1-405b-instruct-mp16" ]]; then
+        PTH_FILE_COUNT=15
+        MODEL_PATH="Meta-Llama-3.1-405B-Instruct-MP16"
+    elif [[ $m == "meta-llama-3.1-405b-instruct-mp8" ]]; then
+        PTH_FILE_COUNT=7
+        MODEL_PATH="Meta-Llama-3.1-405B-Instruct-MP8"
+    elif [[ $m == "meta-llama-3.1-405b-instruct" ]]; then
+        PTH_FILE_COUNT=7
+        MODEL_PATH="Meta-Llama-3.1-405B-Instruct"
+        ADDITIONAL_FILES="fp8_scales_0.pt,fp8_scales_1.pt,fp8_scales_2.pt,fp8_scales_3.pt,fp8_scales_4.pt,fp8_scales_5.pt,fp8_scales_6.pt,fp8_scales_7.pt"
+    elif [[ $m == "meta-llama-3.1-405b-mp16" ]]; then
+        PTH_FILE_COUNT=15
+        MODEL_PATH="Meta-Llama-3.1-405B-MP16"
+        ADDITIONAL_FILES="enablement_code.zip"
+    elif [[ $m == "meta-llama-3.1-405b-mp8" ]]; then
+        PTH_FILE_COUNT=7
+        MODEL_PATH="Meta-Llama-3.1-405B-MP8"
+    elif [[ $m == "meta-llama-3.1-405b" ]]; then
+        PTH_FILE_COUNT=7
+        MODEL_PATH="Meta-Llama-3.1-405B"
+    elif [[ $m == "meta-llama-3.1-70b-instruct" ]]; then
+        PTH_FILE_COUNT=7
+        MODEL_PATH="Meta-Llama-3.1-70B-Instruct"
+    elif [[ $m == "meta-llama-3.1-70b" ]]; then
+        PTH_FILE_COUNT=7
+        MODEL_PATH="Meta-Llama-3.1-70B"
+        ADDITIONAL_FILES="enablement_code.zip"
+    elif [[ $m == "meta-llama-3.1-8b-instruct" ]]; then
+        PTH_FILE_COUNT=0
+        MODEL_PATH="Meta-Llama-3.1-8B-Instruct"
+    elif [[ $m == "meta-llama-3.1-8b" ]]; then
+        PTH_FILE_COUNT=0
+        MODEL_PATH="Meta-Llama-3.1-8B"
+        ADDITIONAL_FILES="enablement_code.zip"
+    elif [[ $m == "meta-llama-guard-3-8b-int8-hf" ]]; then
+        PTH_FILE_COUNT=-1
+        MODEL_PATH="Meta-Llama-Guard-3-8B-INT8-HF"
+        ADDITIONAL_FILES="generation_config.json,model-00001-of-00002.safetensors,model-00002-of-00002.safetensors,model.safetensors.index.json,special_tokens_map.json,tokenizer_config.json,tokenizer.json"
+        TOKENIZER_MODEL=0
+    elif [[ $m == "meta-llama-guard-3-8b" ]]; then
+        PTH_FILE_COUNT=0
+        MODEL_PATH="Meta-Llama-Guard-3-8B"
+    elif [[ $m == "prompt-guard" ]]; then
+        PTH_FILE_COUNT=-1
+        MODEL_PATH="Prompt-Guard"
+        ADDITIONAL_FILES="model.safetensors,special_tokens_map.json,tokenizer_config.json,tokenizer.json"
+        TOKENIZER_MODEL=0
+    fi
+
+
+    printf "\n***Downloading ${MODEL_PATH}***\n"
+    mkdir -p ${TARGET_FOLDER}"/${MODEL_PATH}"
+
+    if [[ $TOKENIZER_MODEL == 1 ]]; then
+        printf "Downloading tokenizer\n"
+        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/tokenizer.model"
+    fi
+
+
+    for s in $(seq -f "0%g" 0 ${PTH_FILE_COUNT})
+    do
+        printf "Downloading consolidated.${s}.pth\n"
+        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/consolidated.${s}.pth"
+    done
+
+    for ADDITIONAL_FILE in ${ADDITIONAL_FILES//,/ }
+    do
+        printf "Downloading $ADDITIONAL_FILE...\n"
+        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/${ADDITIONAL_FILE}"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/${ADDITIONAL_FILE}"
+    done
+
+    if [[ $m != "prompt-guard"   &&  $m != "meta-llama-guard-3-8b-int8-hf" ]]; then
+        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"} -O ${TARGET_FOLDER}"/${MODEL_PATH}/params.json"
+    fi
+done
