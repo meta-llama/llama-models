c5cc65053f83430683ac02a545ace7a01492eeac
Dalton Flanagan <6599399+dltn@users.noreply.github.com>
2024-08-08 16:38:18 -0400

    Merge distros to main (#97)
    * Remove torch and fairscale dependencies
    
    Move `model.py` out of `api` into `reference_impl` so downstream
    packages can take on any required dependencies and this package can stay
    lean
    
    * Remove direct dependency on json-strong-typing by copying a little code
    
    * update gitignore
    
    * Make sure the new directory is picked up in packagin
    
    ---------
    
    Co-authored-by: Ashwin Bharambe <ashwin.bharambe@gmail.com>
    Co-authored-by: Hardik Shah <hjshah@fb.com>
    Co-authored-by: Dalton Flanagan <dalton@fb.com>

--- models/llama3_1/reference_impl/__init__.py
+++ models/llama3_1/reference_impl/__init__.py
@@ -4,8 +4,3 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # top-level folder for each specific model found within the models/ directory at
 # the top-level of this source tree.
-
-from .args import *  # noqa
-from .chat_format import *  # noqa
-from .datatypes import *  # noqa
-from .tokenizer import *  # noqa
