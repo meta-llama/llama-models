46bf7f1c0e4319a0b1a9e3748339d6fa8ef13ed3
Ashwin Bharambe <ashwin.bharambe@gmail.com>
2024-09-17 20:07:59 -0700

    Bump version to 0.0.18

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read_requirements():
 
 setup(
     name="llama_models",
-    version="0.0.17",
+    version="0.0.18",
     author="Meta Llama",
     author_email="llama-oss@meta.com",
     description="Llama models",
