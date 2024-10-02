e46a7086cbe75bb804fdf8070d964aa5d97349e9
anordin95 <alexander.f.nordin@gmail.com>
2024-10-02 11:34:12 -0700

    torch.triu is buggy on device='mps'. Add a workaround.

--- models/llama3/reference_impl/model.py
+++ models/llama3/reference_impl/model.py
@@ -313,6 +313,12 @@ class Transformer(nn.Module):
 
             mask = torch.triu(mask, diagonal=1)
 
+            # https://github.com/pytorch/pytorch/issues/100005
+            # torch.triu is buggy when the device is mps: filled values are 
+            # nan instead of 0. 
+            if mask.device.type == torch.device('mps').type:
+                mask = torch.nan_to_num(mask, nan=0.0)
+
             # When performing key-value caching, we compute the attention scores
             # only for the new sequence. Thus, the matrix of scores is of size
             # (seqlen, cache_len + seqlen), and the only masked entries are (i, j) for
